fbe3950bfe3d7e29ac9b6f84b202337944aee925
Harsh Patel <harshnp02@gmail.com>
2016-11-02 17:07:27 -0400

    added basic foundations

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,20 @@
-//this is where the game will be added
+var userChoice = prompt("Which squad shall we send?[rock][paper][scissors]");
+if(userChoice === rock) {
+  userChoice = "Rock Squad"
+}
+if(userChoice === paper) {
+  userChoice = "Paper Squad"
+} else {
+  userChoice = "Scissor Squad"
+}
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "Rock Squad";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "Paper Squad";
+} else {
+	computerChoice = "Scissor Squad";
+} alert("The enemy general sent forth their" + computerChoice);
+if(userChoice === computerChoice) {
+  alert("tie")
+}//this will be expanded upon
